2ff7fc0465786c2ecd997327edc79192ca8d9b9c
Rampatil1305 <ramanagoudsp13@gmail.com>
2025-06-10 00:56:55 +0530

    java3

---
+++ positive1.java
@@ -0,0 +1,17 @@
+public class positive1 {
+    
+    public static void main(String[] args){
+        int number = 10; // Example number to check
+
+        // Check if the number is positive
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+        
+       
+    }
+}
