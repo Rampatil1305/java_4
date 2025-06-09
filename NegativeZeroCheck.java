cf0eed3543dc42189fcbfce28dc18bb8f532fd1c
Rampatil1305 <ramanagoudsp13@gmail.com>
2025-06-10 01:03:42 +0530

    jai sri ram

---
+++ NegativeZeroCheck.java
@@ -0,0 +1,11 @@
+public class NegativeZeroCheck {
+    public static void main(String[] args) {
+        int num = -5;
+
+        if (num < 0) {
+            System.out.println("Negative");
+        } else if (num == 0) {
+            System.out.println("Zero");
+ }
+}
+}
